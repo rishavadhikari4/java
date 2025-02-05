4627ac4ca67b1bd9fecc1ba74a36345bd322a601
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-02-05 17:00:34 +0545

    added the escape sequence

---
+++ EscapeSequence.java
@@ -0,0 +1,22 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+     
+        System.out.println("This is a new line:\nHello, World!");
+
+        System.out.println("Hello\tWorld");
+
+        System.out.println("Hello\rWorld"); 
+
+        System.out.println("This is a backslash: \\");
+
+        System.out.println("This is a single quote: \'");
+
+        System.out.println("This is a double quote: \"");
+        
+        System.out.println("Hello\bWorld");
+
+        System.out.println("This is a form feed:\fNext line");
+
+        System.out.println("Unicode character: \u0052\u0069\u0073\u0068\u0061\u0076"); // Prints 'Hello'
+    }
+}
