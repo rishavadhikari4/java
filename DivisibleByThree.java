377309c54f1b6dae03bd189c16708536ffccd48d
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-02-14 10:13:41 +0545

    added some changes in the divisible by three

--- DivisibleByThree.java
+++ DivisibleByThree.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
 public class DivisibleByThree {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many you want to print");
+        int n = sc.nextInt();
         int count = 0;
         int num = 1;
         
-        while (count < 10) {
+        while (count < n) {
             if (num % 3 == 0) {
                 System.out.println(num);
                 count++;
             }
             num++;
         }
+        sc.close();
     }
 }
