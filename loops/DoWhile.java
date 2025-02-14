5682c0764b92b94c2ef3343320841abfa3ab9594
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-02-14 10:40:50 +0545

    added the loop

---
+++ loops/DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile{
+    public static void main(String[] arg){
+        //this executes the do while loop once every time,it is an exit control loop 
+        //the below code is an example of the do while loops 
+        int i=1;
+        do
+        {
+            System.out.println(i++);
+        }while(i<=3);
+    }
+}
